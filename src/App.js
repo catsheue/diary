b268bb92363fe3359c011151e24c07322e9a0766
Luna Goodnight <catsheue@gmail.com>
2021-05-18 16:57:31 +0800

    [Add] init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import jan3th from "./styles/images/20210103.webp";
+import feb25 from "./styles/images/20210225.webp";
+import mar6th from "./styles/images/20210306.webp";
+
+const AppWrapper = styled.div`
+  width: 100%;
+  //height: 10rem;
+  height: 10vh;
+  background: teal;
+`;
+const Header = styled.header`
+  background: pink;
+  font-weight: bold;
+  font-size: 2rem;
+  padding: 1rem;
+  text-align: center;
+  height: 10vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const BodyWrapper = styled.div`
+  background: darkslateblue;
+  min-height: 90vh;
+`;
+
+const DiaryWrapper = styled.div`
+  width: 100%;
+  text-align: center;
+  img {
+    margin: 1rem 0;
+    max-width: 80%;
+    aspect-ratio: auto 600 / 140;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <Header>
+        <div>🌺Luna's diary</div>
+        {/*<HeaderNav>111</HeaderNav>*/}
+      </Header>
+      <BodyWrapper>
+        <DiaryWrapper>
+          <img src={jan3th} />
+          <img src={feb25} />
+          <img src={mar6th} />
+        </DiaryWrapper>
+      </BodyWrapper>
+    </AppWrapper>
   );
 }
 
